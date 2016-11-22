1d4bec8183171e70f611f22d442faf4baaf8d75b
Chris Phillips <chris.phillips@canarie.ca>
2016-11-21 20:06:28 -0500

    updated to new overlay technique
    leveraging ability to be more abstracted in where the app can be hosted

--- do_overlay.sh
+++ do_overlay.sh
@@ -4,9 +4,9 @@ set -e
 set -x
 
 # Source the build variables of the container so we can be abstracted
-. /var/www/env
+. /root/env
 
-WORKDIR="/var/www/_work"
+WORKDIR="${CDS_BASE}/_work"
 
 # it is implied that the do_overlay.sh is invoked inside at the root of overlay
 MYPATH="${PWD}"
@@ -15,15 +15,17 @@ SRC_DSROOT="${MYPATH}/assets/dsroot"
 ##
 ## do the 'act of overlaying'
 ##
+#move actual DS php executable to our legacy location
+mv ${CDS_HTMLROOTDIR}/${CDS_HTMLWAYFDIR}/${CDS_WAYFORIGINFILENAME} ${CDS_HTMLROOTDIR}/${CDS_HTMLWAYFDIR}/${CDS_WAYFDESTFILENAME}
 
 # update the images directory
 
-cp ${SRC_DSROOT}/images/logo.png ${CDS_HTMLWAYFDIR}/images/
+cp ${SRC_DSROOT}/images/logo.png ${CDS_HTMLROOTDIR}/${CDS_HTMLWAYFDIR}/images/
 
-cp ${SRC_DSROOT}/css/default-styles.css ${CDS_HTMLWAYFDIR}/css/
+cp ${SRC_DSROOT}/css/default-styles.css ${CDS_HTMLROOTDIR}/${CDS_HTMLWAYFDIR}/css/
 
 # overlay PHP files
-cp ${SRC_DSROOT}/*.php ${CDS_HTMLWAYFDIR}/ 
+cp ${SRC_DSROOT}/*.php ${CDS_HTMLROOTDIR}/${CDS_HTMLWAYFDIR}/ 
 
 
 
